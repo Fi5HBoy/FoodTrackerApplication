bd94211821ebaeb69433919c83a98638e4302467
Fi5HBoy <me@jaykorhonen.com>
2019-09-25 18:30:12 -0500

    initial commit

---
+++ src/test/java/com/jaykorhonen/foodtracker/FoodTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jaykorhonen.foodtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
